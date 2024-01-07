795faee200d62dca48b6f0ce255b7232ee296de7
mik284 <mikeykamau222@gmail.com>
2024-01-07 21:10:37 +0300

    Implemented dependecy injection :@Service

--- src/main/java/com/studentapi/studentapi/student/StudentService.java
+++ src/main/java/com/studentapi/studentapi/student/StudentService.java
@@ -1,11 +1,15 @@
 package com.studentapi.studentapi.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+//@Component
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(

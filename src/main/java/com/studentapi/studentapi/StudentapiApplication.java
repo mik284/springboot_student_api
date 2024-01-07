8e1576f1bf95d43947b4eab3e4db6f51bff54ee2
mik284 <mikeykamau222@gmail.com>
2024-01-07 20:34:21 +0300

    created class student

--- src/main/java/com/studentapi/studentapi/StudentapiApplication.java
+++ src/main/java/com/studentapi/studentapi/StudentapiApplication.java
@@ -2,7 +2,9 @@ package com.studentapi.studentapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class StudentapiApplication {
 

add14fa7980fc05a790cd454d9adb493ec4738fc
mik284 <mikeykamau222@gmail.com>
2024-01-07 20:43:45 +0300

    add student api

--- src/main/java/com/studentapi/studentapi/StudentapiApplication.java
+++ src/main/java/com/studentapi/studentapi/StudentapiApplication.java
@@ -1,8 +1,15 @@
 package com.studentapi.studentapi;
 
+import com.studentapi.studentapi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @RestController
 @SpringBootApplication
@@ -11,5 +18,8 @@ public class StudentapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentapiApplication.class, args);
 	}
-
+	@GetMapping
+	public List<Student> student(){
+		return List.of(new Student(1L, "Munyui", "munyui@gmail.com", LocalDate.of( 2000, Month.APRIL, 12 ), 24 ));
+	}
 }

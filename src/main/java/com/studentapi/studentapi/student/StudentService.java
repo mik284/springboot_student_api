f35b11bd10ed858790a6bc6e9674e4dbbc33b1a5
mik284 <mikeykamau222@gmail.com>
2024-01-10 23:27:29 +0300

    modify repo add to service

--- src/main/java/com/studentapi/studentapi/student/StudentService.java
+++ src/main/java/com/studentapi/studentapi/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.studentapi.studentapi.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,16 @@ import java.util.List;
 //@Component
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Munyui",
-                        "munyui@gmail.com",
-                        LocalDate.of( 2000, Month.APRIL, 12 ),
-                        24
-                ) );
+        return studentRepository.findAll();
+
     }
 }

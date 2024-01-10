63b051c8609de5b4aecaa0f56b5c2b5692f038ac
mik284 <mikeykamau222@gmail.com>
2024-01-10 23:40:45 +0300

    modify student repo

--- src/main/java/com/studentapi/studentapi/student/StudentConfig.java
+++ src/main/java/com/studentapi/studentapi/student/StudentConfig.java
@@ -1,7 +1,36 @@
 package com.studentapi.studentapi.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
 
 @Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student munyui = new Student(
+                    "Munyui",
+                    "munyui@gmail.com",
+                    LocalDate.of(2000, APRIL, 12),
+                    24
+            );
+
+            Student eric = new Student(
+                    "eric",
+                    "eric@gmail.com",
+                    LocalDate.of(2004, APRIL, 12),
+                    24
+            );
+
+            repository.saveAll(List.of(munyui, eric));
+        };
+    }
 }

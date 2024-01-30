57633a8d3cf02f86f66e477cb1ba20bf6e5d25a8
mik284 <mikeykamau222@gmail.com>
2024-01-30 07:23:09 +0300

    add post mapping

--- src/main/java/com/studentapi/studentapi/student/StudentService.java
+++ src/main/java/com/studentapi/studentapi/student/StudentService.java
@@ -24,4 +24,8 @@ public class StudentService {
         return studentRepository.findAll();
 
     }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
